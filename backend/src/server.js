4ada073ecf6b2198915876b6f3cbda070b45c907
Anshul Porwal <anshul.porwal@adixoo.com>
2025-09-14 19:50:16 +0530

    Signup code refactoring

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js"
 import {connectDb} from "./lib/db.js"
 
@@ -10,12 +11,16 @@ const app=express();
 
 //middleware to acess the users payload
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 
 const PORT=process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}` );
-    connectDb();
-})
+connectDb().then(()=>{
+   app.listen(PORT,()=>{
+        console.log(`Server running on port ${PORT}` );
+    })
+}).catch((error)=>{
+    console.log("Error connecting to database",error);
+});
